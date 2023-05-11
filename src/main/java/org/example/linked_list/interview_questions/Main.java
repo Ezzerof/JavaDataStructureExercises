b69eab50587bee573d336359d938d1ebfba0ce83
Ezzerof <christian.bitca@gmail.com>
2023-05-11 10:50:39 +0100

    Find middle node operation

---
+++ src/main/java/org/example/linked_list/interview_questions/Main.java
@@ -0,0 +1,22 @@
+package org.example.linked_list.interview_questions;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList myList = new LinkedList(1);
+
+        myList.append(2);
+        myList.append(3);
+        myList.append(4);
+        myList.append(5);
+        LinkedList.Node middleNode = myList.findMiddleNode();
+        System.out.println(middleNode.value); // Output: 3
+
+        myList.append(6);
+        middleNode = myList.findMiddleNode();
+        System.out.println(middleNode.value); // Output: 4
+
+    }
+
+}
